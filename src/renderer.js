183a390dafeb958c6949cb4ce110b41b63207537
iansmith1337 <iansmith1337@gmail.com>
2022-11-07 15:42:52 -0500

    Get document and append result to ordered list

--- src/renderer.js
+++ src/renderer.js
@@ -44,7 +44,9 @@ import {
   documentId,
   limit,
   FieldPath,
+  exists,
 } from 'firebase/firestore'
+import { map } from '@firebase/util'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyA3gUrQZvp23tQINOnr764EMPTczPlYbUw',
@@ -69,37 +71,68 @@ const DB = getFirestore(FBapp)
 
 console.log('Renderer loaded.')
 
-const listRoot = document.getElementById('tweetList')
-var originalMap = new Map()
-var sortedMap = new Map()
+var lastID = ''
+var executing = false
 
-document.getElementById('UpdateButton').onclick = async function () {
-  const q = new query(
-    collection(DB, 'cycles'),
-    where('Time', '>=', Date.now() / 1000),
-    limit(1),
-  )
-  const querySnapshot = await getDocs(q)
-  console.log(querySnapshot.docs[0].id)
-  const docData = querySnapshot.docs[0].data()
-  console.log(docData)
-  for (const entity in docData) {
-    if (Object.hasOwnProperty.call(docData, entity)) {
-      const element = docData[entity]
-      console.log(docData.entity + ': ' + element)
+const listRoot = document.getElementById('tweetList')
+
+async function getLatestInfo() {
+  var originalMap = new Map()
+  if (!executing) {
+    executing = true
+    const q = new query(
+      collection(DB, 'cycles'),
+      orderBy('Time', 'desc'),
+      limit(1),
+    )
+    const querySnapshot = await getDocs(q)
+    if (querySnapshot.docs.length > 0) {
+      console.log('New doc?: ' + (lastID !== querySnapshot.docs[0].id))
+      if (lastID !== querySnapshot.docs[0].id) {
+        const docData = querySnapshot.docs[0].data()
+        for (const entity in docData) {
+          if (Object.hasOwnProperty.call(docData, entity)) {
+            const element = docData[entity]
+            if (entity !== 'Time') {
+              originalMap.set(entity, element)
+            }
+          }
+        }
+        const sorted = createSortedMap(originalMap)
+        console.log('Map created.')
+        console.log(sorted)
+        updateList(sorted)
+        lastID = querySnapshot.docs[0].id
+      }
     }
+    executing = false
   }
 }
 
-const q = new Query(collection(DB, 'cycles'))
-/*const listener = onSnapshot(q, (snapshot) => {
-  if (snapshot.docChanges().length == 1) {
-    snapshot.docChanges().forEach((change) => {
-      if (change.type === 'added') {
-        for (let i = 0; i < change.doc.data().length; i++) {
-          console.log(change.doc.data()[i])
-        }
-      }
+function createSortedMap(starting) {
+  console.log('Creating map.')
+  return new Map([...starting.entries()].sort((a, b) => b[1] - a[1]))
+}
+
+function updateList(sortedEntities) {
+  console.log('Cleaning list...')
+  if (listRoot.hasChildNodes) {
+    listRoot.childNodes.forEach((e) => {
+      e.remove()
     })
   }
-})*/
+  console.log('Filling...')
+  var count = 0
+  sortedEntities.forEach((item, index) => {
+    if (count < 20) {
+      const numberedItem = document.createElement('li')
+      const NIText = document.createTextNode(index + ': ' + item)
+      listRoot.appendChild(numberedItem)
+      numberedItem.appendChild(NIText)
+      count++
+    }
+  })
+}
+
+document.getElementById('UpdateButton').onclick = getLatestInfo
+setInterval(getLatestInfo, 300000)
