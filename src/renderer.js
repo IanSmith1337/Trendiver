23b054a0f50bd0d7b37e73fdbfc601f60f19fc3b
iansmith1337 <iansmith1337@gmail.com>
2022-11-07 21:08:21 -0500

    Get the list and shit working

--- src/renderer.js
+++ src/renderer.js
@@ -68,42 +68,51 @@ const firebaseConfig = {
 
 const FBapp = initializeApp(firebaseConfig)
 const DB = getFirestore(FBapp)
-
-console.log('Renderer loaded.')
-
-var lastID = ''
+const listRoot = document.createElement('ol')
+const idNode = document.createAttribute('id')
+idNode.value = 'tweetList'
 var executing = false
+var currentL = 1
+var q
 
-const listRoot = document.getElementById('tweetList')
+function setLength(l) {
+  // Time controls for document retreival.
+  // 1 = 5 minutes
+  // 3 = 15 minutes
+  // 6 = 30 minutes
+  // 12 = 1 hour
+  // 36 = 3 hours
+  // 72 = 6 hours
+  // 144 = 12 hours
+  // 288 = 1 day
+  q = new query(collection(DB, 'cycles'), orderBy('Time', 'desc'), limit(l))
+}
 
-async function getLatestInfo() {
+async function callDB() {
   var originalMap = new Map()
   if (!executing) {
     executing = true
-    const q = new query(
-      collection(DB, 'cycles'),
-      orderBy('Time', 'desc'),
-      limit(1),
-    )
     const querySnapshot = await getDocs(q)
     if (querySnapshot.docs.length > 0) {
-      console.log('New doc?: ' + (lastID !== querySnapshot.docs[0].id))
-      if (lastID !== querySnapshot.docs[0].id) {
-        const docData = querySnapshot.docs[0].data()
-        for (const entity in docData) {
-          if (Object.hasOwnProperty.call(docData, entity)) {
-            const element = docData[entity]
+      querySnapshot.docs.forEach((doc) => {
+        const CDdata = doc.data()
+        for (const entity in CDdata) {
+          if (Object.hasOwnProperty.call(CDdata, entity)) {
+            const element = CDdata[entity]
             if (entity !== 'Time') {
-              originalMap.set(entity, element)
+              if (originalMap.has(entity)) {
+                originalMap.set(entity, originalMap.get(entity) + element)
+              } else {
+                originalMap.set(entity, element)
+              }
             }
           }
         }
-        const sorted = createSortedMap(originalMap)
-        console.log('Map created.')
-        console.log(sorted)
-        updateList(sorted)
-        lastID = querySnapshot.docs[0].id
-      }
+      })
+      console.log('Finished gathering data.')
+      const sorted = createSortedMap(originalMap)
+      console.log('Map created.')
+      updateList(sorted)
     }
     executing = false
   }
@@ -116,10 +125,16 @@ function createSortedMap(starting) {
 
 function updateList(sortedEntities) {
   console.log('Cleaning list...')
-  if (listRoot.hasChildNodes) {
-    listRoot.childNodes.forEach((e) => {
-      e.remove()
-    })
+  if (document.getElementById('tweetList') != null) {
+    document.getElementById('tweetList').remove()
+    while (listRoot.firstChild) {
+      listRoot.removeChild(listRoot.firstChild)
+    }
+    document.body.appendChild(listRoot)
+    listRoot.attributes.setNamedItem(idNode)
+  } else {
+    document.body.appendChild(listRoot)
+    listRoot.attributes.setNamedItem(idNode)
   }
   console.log('Filling...')
   var count = 0
@@ -134,5 +149,46 @@ function updateList(sortedEntities) {
   })
 }
 
-document.getElementById('UpdateButton').onclick = getLatestInfo
-setInterval(getLatestInfo, 300000)
+document.getElementById('UpdateButton').onclick = function () {
+  setLength(currentL)
+  callDB()
+  switch (currentL) {
+    case 1:
+      currentL = 3
+      document.getElementById('UpdateButton').textContent = '5 minutes'
+      break
+    case 3:
+      currentL = 6
+      document.getElementById('UpdateButton').textContent = '15 minutes'
+      break
+    case 6:
+      currentL = 12
+      document.getElementById('UpdateButton').textContent = '30 minutes'
+      break
+    case 12:
+      currentL = 36
+      document.getElementById('UpdateButton').textContent = '1 hour'
+      break
+    case 36:
+      currentL = 72
+      document.getElementById('UpdateButton').textContent = '3 hours'
+      break
+    case 72:
+      currentL = 144
+      document.getElementById('UpdateButton').textContent = '6 hours'
+      break
+    case 144:
+      currentL = 288
+      document.getElementById('UpdateButton').textContent = '12 hours'
+      break
+    case 288:
+      currentL = 1
+      document.getElementById('UpdateButton').textContent = '24 hours'
+      break
+    default:
+      currentL = 1
+      break
+  }
+}
+
+console.log('Renderer loaded.')
